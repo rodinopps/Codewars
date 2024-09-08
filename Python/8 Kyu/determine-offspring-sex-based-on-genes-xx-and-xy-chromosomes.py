6186f7ff776df0f9034b2020a17edd474450814d
Rodin Opuz <opuzrodin@gmail.com>
2024-09-08 12:14:30 +0100

    Add files via upload

---
+++ Python/8 Kyu/determine-offspring-sex-based-on-genes-xx-and-xy-chromosomes.py
@@ -0,0 +1,5 @@
+def chromosome_check(chromosome):
+    if chromosome == "XX":
+        return "Congratulations! You're going to have a daughter."
+    else:
+        return "Congratulations! You're going to have a son."
---
+++ Python/8 Kyu/welcome-to-the-city.py
@@ -0,0 +1,3 @@
+def say_hello(name, city, state):
+    full = " ".join(name)
+    return f"Hello, {full}! Welcome to {city}, {state}!"
