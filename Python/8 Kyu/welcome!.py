6ef23dae16b9178f9344a2b40da2e2915f6bfd67
Rodin Opuz <opuzrodin@gmail.com>
2024-09-10 18:44:47 +0100

    Add files via upload

---
+++ Python/8 Kyu/welcome!.py
@@ -0,0 +1,23 @@
+def greet(language):
+    choice = [ ("english", "Welcome")
+, ("czech", "Vitejte")
+, ("danish", "Velkomst")
+, ("dutch", "Welkom")
+, ("estonian", "Tere tulemast")
+, ("finnish", "Tervetuloa")
+, ("flemish", "Welgekomen")
+, ("french", "Bienvenue")
+, ("german", "Willkommen")
+, ("irish", "Failte")
+, ("italian", "Benvenuto")
+, ("latvian", "Gaidits")
+, ("lithuanian", "Laukiamas")
+, ("polish", "Witamy")
+, ("spanish", "Bienvenido")
+, ("swedish", "Valkommen")
+, ("welsh", "Croeso") ]
+    greetings = dict(choice)
+    if language in greetings:
+        return greetings[language]
+    else:
+        return "Welcome"
