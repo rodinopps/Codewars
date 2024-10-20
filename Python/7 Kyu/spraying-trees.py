0c96eac9d5dded3df8cf67d042db5136979f7ecd
Rodin Opuz <opuzrodin@gmail.com>
2024-10-20 13:17:23 +0100

    Create spraying-trees.py

---
+++ Python/7 Kyu/spraying-trees.py
@@ -0,0 +1,3 @@
+def task(w, n, c):
+    weekend = {"Monday": "James", "Tuesday": "John", "Wednesday": "Robert", "Thursday": "Michael", "Friday": "William"}
+    return f"It is {w} today, {weekend[w]}, you have to work, you must spray {n} trees and you need {n * c} dollars to buy liquid"
