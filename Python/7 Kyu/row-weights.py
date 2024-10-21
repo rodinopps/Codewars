337fc5d009bdd93c0eb8258b37d25922883f16d3
Rodin Opuz <opuzrodin@gmail.com>
2024-10-21 10:40:32 +0100

    Update row-weights.py

--- Python/7 Kyu/row-weights.py
+++ Python/7 Kyu/row-weights.py
@@ -1,7 +1,2 @@
 def row_weights(array):
-    team1, team2 = 0, 0
-    for i in range(0, len(array), 2):
-        team1 += array[i]
-    for i in range(1, len(array), 2):
-        team2 += array[i]
-    return (team1, team2)
+    return sum(array[::2]), sum(array[1::2])
