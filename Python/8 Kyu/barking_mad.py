4a504b641e2b76b2d63f2f69b95c5db540c50a13
Rodin Opuz <opuzrodin@gmail.com>
2024-11-23 13:01:21 +0000

    Create barking_mad.py

---
+++ Python/8 Kyu/barking_mad.py
@@ -0,0 +1,7 @@
+class Dog ():
+  def __init__(self, breed):
+    self.breed = breed
+snoopy = Dog("Beagle")
+snoopy.bark = lambda: "Woof"
+scoobydoo = Dog("Great Dane")
+scoobydoo.bark = lambda: "Woof"
