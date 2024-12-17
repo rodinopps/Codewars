b68929e44d3ddaa50df7b1a8c302ea81b1e288e3
Rodin Opuz <opuzrodin@gmail.com>
2024-12-17 23:15:13 +0000

    Create grasshopper_create_the_rooms.py

---
+++ Python/8 Kyu/grasshopper_create_the_rooms.py
@@ -0,0 +1,3 @@
+rooms = {"one": {"name": "room one", "description": "easy", "completed": False},
+        "two": {"name": "room two", "description": "medium", "completed": False},
+        "three": {"name": "room three", "description": "hard", "completed": False}}
